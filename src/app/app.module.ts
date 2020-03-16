36527bab31b56782d33542456d7e52d3d9d8d464
alparslancay <alparslancay1@gmail.com>
2020-03-16 23:15:52 +0300

    Deleted unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { TrialsComponent } from './trials/trials.component';
 import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
 import { FeaturedProductsComponent } from './featured-products/featured-products.component';
-import { GameProductComponent } from './game-product/game-product.component';
-import { ErpProductComponent } from './erp-product/erp-product.component';
 import { SpecialRequestProductComponent } from './special-request-product/special-request-product.component';
 import { ShoppingChartComponent } from './shopping-chart/shopping-chart.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -26,8 +24,6 @@ import { ItemComponent } from './item/item.component';
     TrialsComponent,
     NewArrivalsComponent,
     FeaturedProductsComponent,
-    GameProductComponent,
-    ErpProductComponent,
     SpecialRequestProductComponent,
     ShoppingChartComponent,
     ProductDetailComponent,
